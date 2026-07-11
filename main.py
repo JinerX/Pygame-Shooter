21d3d81d1d6c65ac6004bc6e4614b1895c1b0b4f
Jędrzej Sajnóg <sajnog.jedrzej@gmail.com>
2026-07-11 08:53:46 +0300

    Added player movement

--- main.py
+++ main.py
@@ -7,25 +7,43 @@ WIDTH, HEIGHT = 800, 400
 
 ACTIVE = True
 
-class CharacterTests:
-    def __init__(self) -> None:
-        self.pos = (20,20)
+SPEED_CHANGE = 90
+MAX_SPEED = 9000
 
-    def draw(self, screen):
-        pygame.draw.circle(screen, "Red", self.pos, 10)
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, *groups, deceleration_factor=0.1, max_speed=10000):
+        super().__init__(*groups)
+        self.image = pygame.surface.Surface((20,20), pygame.SRCALPHA)
+        self.rect = self.image.get_rect(center = pos)
+        self.velocity = pygame.Vector2(0,0)
+        self.deceleration_factor = deceleration_factor
+        self.max_speed = max_speed
+        pygame.draw.circle(self.image, "Red", (10,10), 10)
     
-    def update_pos(self, new_pos):
-        self.pos = new_pos
+    def update(self, acc, dt):
+        stopping = pygame.Vector2(- (self.deceleration_factor * self.velocity.x), -(self.deceleration_factor * self.velocity.y))
+        self.velocity += acc + stopping
 
+        if self.velocity.x > self.max_speed:
+            self.velocity.x = self.max_speed
+        if self.velocity.x < -self.max_speed:
+            self.velocity.x = -self.max_speed
+        if self.velocity.y > self.max_speed:
+            self.velocity.y = self.max_speed
+        if self.velocity.y < -self.max_speed:
+            self.velocity.y = -self.max_speed
+        self.rect.x += self.velocity.x * dt
+        self.rect.y += self.velocity.y * dt
 
-
-
-test_char = CharacterTests()
-pos = (20,20)
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("My game")
 clock = pygame.time.Clock()
+pos = (40,40)
+
+player_group = pygame.sprite.GroupSingle()
+player = Player(pos, player_group, max_speed=MAX_SPEED)
 
 while ACTIVE:
     dt = clock.tick(60) / 1000
@@ -33,15 +51,25 @@ while ACTIVE:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    acc = pygame.Vector2(0,0)
     keys = pygame.key.get_pressed()
+    
     if keys[pygame.K_RIGHT]:
-        pos = (pos[0]+1, pos[1])
-        test_char.update_pos(pos)
-        test_char.draw(screen)
+        acc.x += SPEED_CHANGE
+    if keys[pygame.K_LEFT]:
+        acc.x -= SPEED_CHANGE
+    if keys[pygame.K_UP]:
+        acc.y -= SPEED_CHANGE
+    if keys[pygame.K_DOWN]:
+        acc.y += SPEED_CHANGE
+    player_group.update(acc, dt)
 
-        print("Hello world")
-    # pygame.draw.circle(screen, "Red", (40,40), 20)
-    # pygame.display.update()
+
+    # all_sprites.update(pos)
+    screen.fill("blue")
+    player_group.draw(screen)
+
+    pygame.display.update()
 
 
 pygame.quit()
